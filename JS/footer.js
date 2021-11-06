2dcfbd571e1873ea52184ba98105ee51b883e807
anand7071 <59311209+anand7071@users.noreply.github.com>
2021-11-06 14:48:39 +0530

    footer is alomost done

---
+++ JS/footer.js
@@ -0,0 +1,79 @@
+function footer(){
+    return `<div class="navbar">
+    <div class="navbarStrip">
+        <div class="navStripMat">
+            <a id="H1" href="#">Get the App</a>
+            <a id="H1" href="#">For Doctors</a>
+            <a id="H1" href="#">Health Feed</a>
+            <a id="Login" href="#">Login/SingUp</a>
+        </div>
+        <div class="WhiteFill">
+            <div class="navBarLarge">
+                <div class="LybrateLogo">
+                    <a href="#">
+                        <img id="imgLogo" src="https://assets.lybrate.com/q_auto,f_auto//imgs/product/logos/Lybrate-Logo.png">
+                      
+                    </a>
+                </div>
+                <div class="clickSection">
+                    <a href="#">
+                        <div class="ForInline">
+                        <img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Ask-Question.png">
+                        <p>Ask a question</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="clickSection">
+                    <a href="#">
+                        <div class="ForInline">
+                        <img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Book-Appointment.png"">
+                        <p>Book Appointment</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="clickSection">
+                    <a href="#">
+                        <div class="ForInline">
+                        <img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Consult-Online.png">
+                        <p>Counsult Online</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="clickSection">
+                    <a href="#">
+                        <div class="ForInline">
+                        <img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Book-Lab-test.png">
+                        <p>Book Lab Test</p>
+                        </div>
+                    </a>
+                </div>
+                <div class="GotocartBtn">
+                    <a href="#">
+                        Lybrate Good Kart
+                    </a>
+                </div>
+                <div class="cartbtnlogo"></div>
+            </div>
+        </div>
+        <div class="Aline"></div>
+        
+        <div class="WhiteFill">
+            <div class="navBarSmall">
+                <div class="navStripMat" id="NavStripMatId">
+                    
+                    <a class="Anchor1" href="#">Home</a>
+                    <a class="Anchor1" href="#">Health Feed</a>
+                    <a class="Anchor1" href="#">GoodKart</a>
+                    <a class="Anchor1" href="#">Ask a question</a>
+                    <a class="Anchor1" href="#">Consult online</a>
+                    <a class="Anchor1" href="#">Book Lab Test</a>
+                    <a class="Anchor1" href="#">Gold membershhip</a>
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>`
+}
+
+export default footer();
---
+++ JS/index.js
@@ -0,0 +1,3 @@
+import footer from './footer.js'
+console.log(footer);
+document.getElementById("footer").innerHTML = footer;
