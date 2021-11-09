68a378360d6cff4183492434fa6cc779b8f4977b
bobbymore2025 <88192206+bobbymore2025@users.noreply.github.com>
2021-11-09 19:35:35 +0530

    search function done

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const PORT = 1000
+const PORT = 2000
 
 const path = require('path');
 
@@ -6,7 +6,7 @@ const jsonServer = require('json-server');
 
 const server = jsonServer.create()
 
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
+const router = jsonServer.router(path.join(__dirname, 'database.json'))
 
 const middlewares = jsonServer.defaults()
 
