033a56833e931bfea106d8f10dc9a185b17b6db3
Mohit Sehrawat <mohitsehrawat000@gmail.com>
2021-11-11 21:58:13 +0530

    navbar code changed

--- frontend/JS/topHeader.js
+++ frontend/JS/topHeader.js
@@ -1,34 +1,41 @@
-async function topHeader(){
-    return `<!-- Header Code start-->
-
-    <div id="topnavbar">
-        <div class="conatiner-fluid topnavbar">
-            <div class="row">
-                <div class="col-12 col-sm-12 col-md-2 col-lg-3"></div>
-                <div class="col-12 col-sm-12 col-md-2 col-lg-2">Get theApp</div>
-                <div class="col-12 col-sm-12 col-md-2 col-lg-2 ">for Doctors</div>
-                <div class="col-12 col-sm-12 col-md-2 col-lg-2 ">HealthFeed</div>
-                <div class="col-12 col-sm-12 col-md-2 col-lg-2 "><a style="text-decoration: none;color: rgb(105, 93, 93); " href="signup.html">Hello,User</a></div>
-                <div class="col-12 col-sm-12 col-md-2 col-lg-1 "></div>
-            </div>
+async function topHeader() {
+    return `<div id="topnavbar">
+    <div class="conatiner-fluid topnavbar">
+        <div class="row">
+            <div class="col-12 col-sm-12 col-md-2 col-lg-3"></div>
+            <div style="cursor: pointer;" title="App will come soon" class="col-12 col-sm-12 col-md-2 col-lg-2">Get theApp</div>
+            <div onclick="bookAppointment('diabetes')" style="cursor: pointer;" class="col-12 col-sm-12 col-md-2 col-lg-2 ">for Doctors</div>
+            <div onclick="displayNews('depression')" style="cursor: pointer;" class="col-12 col-sm-12 col-md-2 col-lg-2 ">HealthFeed</div>
+            <div class="col-12 col-sm-12 col-md-2 col-lg-2 "><a style="text-decoration: none;color: rgb(105, 93, 93); " href="signup.html">Login/Signup</a></div>
+            <div class="col-12 col-sm-12 col-md-2 col-lg-1 "></div>
         </div>
     </div>
+</div>
 
-    <div id="navbar">
-
-        <a href="landingpage.html"><img style="width: 10%; height: 35px;" src="https://assets.lybrate.com/q_auto,f_auto//imgs/product/logos/Lybrate-Logo.png"></a>
-        <a href=""><img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Ask-Question.png" alt="">Ask Question</a>
-        <a href=""><img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Book-Appointment.png" alt="">Book Appointment</a>
-        <a href=""><img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Consult-Online.png" alt="">Consult Online</a>
-        <a href=""><img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Book-Lab-test.png" alt="">Book Lab Test</a>
-        <a href=""><button style="padding-top: 1px;"><span style="font-size: 20px;  padding-top: 0px; ">goodkart</span></button></a>
-        <a href=""><img style="color: red; height: 50px; width: 4%;" src="https://github.com/adyasha9/medical-website-frontend/blob/master/lybrate-homepage/cart.png?raw=true" alt=""></a>
-
+<div id="navbar">
+    <div class="mt-2">
+        <a href="landingpage.html"><img style="width: 27%; margin-left: 30%;" src="https://assets.lybrate.com/q_auto,f_auto//imgs/product/logos/Lybrate-Logo.png"></a>
     </div>
-
-    <hr style="margin-top: 5px;">
-
-    <!-- Header Code end -->`
+    <div onclick="bookAppointment('depression')" class="mt-3" style="cursor: pointer;">
+        <a><img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Ask-Question.png">Ask Question</a>
+    </div>
+    <div onclick="bookAppointment('hairfall')" class="mt-3" style="cursor: pointer;">
+        <a><img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Book-Appointment.png" alt="">Book Appointment</a>
+    </div>
+    <div class="mt-3">
+        <a href="consultOnline.html"><img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Consult-Online.png" alt="">Consult Online</a>
+    </div>
+    <div onclick="bookAppointment('diabetes')" class="mt-3" style="cursor: pointer;">
+        <a><img src="https://assets.lybrate.com/q_auto,f_auto,h_28,w_28,c_fill/imgs/product/Homepage/Book-Lab-test.png" alt="">Book Lab Test</a>
+    </div>
+    <div class="mt-2">
+        <a href="goodkart.html"><button class="btn btn-danger rounded-pill">goodkart</button></a>
+    </div>
+    <div class="mt-1">
+        <a href="cart.html"><img style="width:80%;" src="https://github.com/adyasha9/medical-website-frontend/blob/master/lybrate-homepage/cart.png?raw=true" alt=""></a>
+    </div>
+</div>
+<hr style="margin-top: 3px;">`
 }
 
 export default topHeader;
